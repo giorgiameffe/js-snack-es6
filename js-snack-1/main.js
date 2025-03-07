9a8585cdd0afbee2d97e434113d867ed9f1156b6
giorgiameffe <giorgiameffe03@gmail.com>
2025-03-07 17:54:41 +0100

    feat: create objects array

---
+++ js-snack-1/main.js
@@ -0,0 +1,29 @@
+
+// creare un array di oggetti 
+
+const bikes = [
+
+    {
+        name: 'Bianchi',
+        weight: 7.2
+    },
+    {
+        name: 'Bmc',
+        weight: 7.5
+    },
+    {
+        name: 'Pinarello',
+        weight: 7.8
+    },
+    {
+        name: 'Cinelli',
+        weight: 8
+    }
+    
+]
+
+console.log(bikes);
+
+
+
+
