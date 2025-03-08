3625a5529aaedb1ec8a590abe5f3b74b497894e5
giorgiameffe <giorgiameffe03@gmail.com>
2025-03-08 16:25:58 +0100

    feat: complete snack 1

--- js-snack-1/main.js
+++ js-snack-1/main.js
@@ -5,7 +5,7 @@ const bikes = [
 
     {
         name: 'Bianchi',
-        weight: 7.2
+        weight: 7.6
     },
     {
         name: 'Bmc',
@@ -23,6 +23,42 @@ const bikes = [
 ]
 
 console.log(bikes);
+console.log(bikes.length);
+
+// creare array vuoto con i rispettivi pesi delle bici
+
+const bikesWeight = [];
+
+for (let i = 0; i < bikes.length; i++) {
+
+    const currentWeight = bikes[i].weight;
+    bikesWeight.push(currentWeight);
+
+}
+
+console.log(bikesWeight);
+
+// sort --> ordina gli elemtenti dell'array dal numero più basso al numero più alto
+
+bikesWeight.sort();
+
+// trovare la bici con il peso minore mettendo a confronto i due array
+
+for (let i = 0; i < bikes.length; i++) {
+
+    const currentWeight = bikes[i].weight;
+    // console.log(currentWeight);
+    const currentName = bikes[i].name;
+    // console.log(currentName);
+
+    if (bikesWeight[0] === currentWeight) {
+
+        console.log(`La bici con il peso minore è ${currentName}, il suo peso è di ${currentWeight} kg`)
+    } 
+}
+
+
+
 
 
 
