f353ad8930b5db8e9981a6efdd5745b4c0c0dcfc
giorgiameffe <giorgiameffe03@gmail.com>
2025-03-09 16:48:53 +0100

    feat: create objects array

---
+++ js-snack-2/main.js
@@ -0,0 +1,31 @@
+
+// creare un array di oggetti di squadre di calcio
+
+const footballTeams = [
+
+    {
+        team: 'Lazio',
+        fouls: 0,
+        goals: 0
+    },
+    {
+        team: 'Torino',
+        fouls: 0,
+        goals: 0
+    },
+    {
+        team: 'Inter',
+        fouls: 0,
+        goals: 0
+    },
+    {
+        team: 'Atalanta',
+        fouls: 0,
+        goals: 0
+    },
+    {
+        team: 'Napoli',
+        fouls: 0,
+        goals: 0
+    }  
+]
