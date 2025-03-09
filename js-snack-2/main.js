1bdea2773f1930d88da0bf8dfb6d8ef5e389fde5
giorgiameffe <giorgiameffe03@gmail.com>
2025-03-09 21:55:38 +0100

    feat: add fouls and goals random numbers

--- js-snack-2/main.js
+++ js-snack-2/main.js
@@ -29,3 +29,27 @@ const footballTeams = [
         goals: 0
     }  
 ]
+
+// creare una funzione per generare numeri randomici
+
+function generateRandomNumbers (min,max) {
+
+    const randomNumbers = Math.floor(Math.random() * (max - min + 1) + min);
+    return randomNumbers;  
+}
+
+// iterare l'array per assegnare a ciascuna chiave i numeri randomici come valore
+
+for (let i = 0; i < footballTeams.length; i++) {
+
+    const teamsFouls = generateRandomNumbers(0,10);
+    const teamsGoals = generateRandomNumbers(0,10);
+    footballTeams[i].fouls = teamsFouls;
+    footballTeams[i].goals = teamsGoals;
+}
+
+console.log(footballTeams);
+
+
+
+
